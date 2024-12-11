f4a7690fe6f54e963d0e11d3f552d45f0ed56bfe
Victor Mazzola <victormazzola2019@gmail.com>
2024-12-11 06:58:33 -0300

    add cpf.js

---
+++ js/cpf.js
@@ -0,0 +1,23 @@
+export default function isCPF(field) {
+    const cpf = field.value.replace(/\.|-/g, "");
+    validateRepeatedNumber(cpf);
+
+    console.log(validateRepeatedNumber(cpf));
+}
+
+function validateRepeatedNumber(cpf) {
+    const repeatedNumbers = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+    ]
+
+    return repeatedNumbers.includes(cpf)
+}
