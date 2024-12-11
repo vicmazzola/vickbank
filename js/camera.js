f74dc73405a195380235abfa1755409a6af615ef
Victor Mazzola <victormazzola2019@gmail.com>
2024-12-11 07:26:40 -0300

    add camera.js update script.js

---
+++ js/camera.js
@@ -0,0 +1,39 @@
+const btnStartCamera = document.querySelector("[data-video-button]");
+const cameraField = document.querySelector("[data-camera]");
+const video = document.querySelector("[data-video]");
+const btnTakePhoto = document.querySelector("[data-take-photo]");
+const canvas = document.querySelector("[data-video-canvas]");
+const message = document.querySelector("[data-message]");
+const btnSendPhoto = document.querySelector("[data-send]");
+
+let imageURL = '';
+
+btnStartCamera.addEventListener('click', async function () {
+    const startVideo = await navigator.mediaDevices
+        .getUserMedia({ video: true, audio: false });
+
+    btnStartCamera.style.display = "none";
+    cameraField.style.display = "block";
+
+    video.srcObject = startVideo;
+});
+
+btnTakePhoto.addEventListener('click', function () {
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    imageURL = canvas.toDataURL('image/jpeg');
+
+    cameraField.style.display = "none";
+    message.style.display = "block";
+});
+
+btnSendPhoto.addEventListener('click', () => {
+    const existingData = localStorage.getItem("registration");
+    const parsedData = JSON.parse(existingData);
+
+    parsedData.image = imageURL;
+
+    localStorage.setItem('registration', JSON.stringify(parsedData));
+
+    window.location.href = '../pages/open-account-form-3.html';
+});
--- js/script.js
+++ js/script.js
@@ -1,19 +1,93 @@
 import isCPF from "./cpf.js";
 import isLegalAge from "./legal-age.js";
 
-const formField = document.querySelectorAll('[required]')
+const formFields = document.querySelectorAll('[required]');
+const form = document.querySelector('[data-form]');
 
-formField.forEach((field) => {
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const responsesList = {
+        "name": e.target.elements["name"].value,
+        "email": e.target.elements["email"].value,
+        "rg": e.target.elements["rg"].value,
+        "cpf": e.target.elements["cpf"].value,
+        "birthday": e.target.elements["birthday"].value,
+    };
+
+    localStorage.setItem("registration", JSON.stringify(responsesList));
+
+    window.location.href = "./open-account-form-2.html";
+});
+
+formFields.forEach((field) => {
     field.addEventListener("blur", () => checkField(field));
-})
+    field.addEventListener("invalid", (event) => event.preventDefault());
+});
+
+const errorTypes = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+    'tooShort',
+    'customError'
+];
+
+const messages = {
+    name: {
+        valueMissing: "The name field cannot be empty.",
+        patternMismatch: "Please provide a valid name.",
+        tooShort: "Please provide a valid name."
+    },
+    email: {
+        valueMissing: "The email field cannot be empty.",
+        typeMismatch: "Please provide a valid email.",
+        tooShort: "Please provide a valid email."
+    },
+    rg: {
+        valueMissing: "The RG field cannot be empty.",
+        patternMismatch: "Please provide a valid RG.",
+        tooShort: "The RG field does not have enough characters."
+    },
+    cpf: {
+        valueMissing: "The CPF field cannot be empty.",
+        patternMismatch: "Please provide a valid CPF.",
+        customError: "The CPF provided does not exist.",
+        tooShort: "The CPF field does not have enough characters."
+    },
+    birthday: {
+        valueMissing: "The birthday field cannot be empty.",
+        customError: "You must be over 18 years old to register."
+    },
+    terms: {
+        valueMissing: "You must accept our terms before continuing."
+    }
+};
 
 function checkField(field) {
+    let message = "";
+    field.setCustomValidity('');
+
     if (field.name == "cpf" && field.value.length >= 11) {
         isCPF(field);
     }
-
     if (field.name == "birthday" && field.value != "") {
         isLegalAge(field);
     }
 
+    errorTypes.forEach((error) => {
+        if (field.validity[error]) {
+            message = messages[field.name][error];
+            console.log(message);
+        }
+    });
+
+    const errorMessage = field.parentNode.querySelector('.error-message');
+    const isValid = field.checkValidity();
+
+    if (!isValid) {
+        errorMessage.textContent = message;
+    } else {
+        errorMessage.textContent = "";
+    }
 }
